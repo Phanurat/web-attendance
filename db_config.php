1976981aa5d2a0e669d5d2e7a080d02036685010
Phanurat <paunavt5@gmail.com>
2025-04-10 08:41:44 +0700

    update ,env

--- db_config.php
+++ db_config.php
@@ -1,8 +1,26 @@
 <?php
-$servername = "localhost";  // ชื่อของ service ใน docker-compose.yml
-$username = "root";
-$password = "";
-$dbname = "attendance";  // ชื่อฐานข้อมูลที่ใช้
+// สำหรับโหลด .env ใน Local
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
+
+// ตรวจว่า environment ไหน
+$appEnv = getenv('APP_ENV') ?: 'local';
+
+if ($appEnv === 'local') {
+    $servername = getenv("DB_HOST") ?: 'localhost';
+    $dbname     = getenv("DB_NAME") ?: 'attendance';
+    $username   = getenv("DB_USER") ?: 'root';
+    $password   = getenv("DB_PASS") ?: '';
+} else {
+    // สำหรับ Docker
+    $servername = getenv("DB_HOST") ?: 'db'; // ชื่อ service ใน docker-compose
+    $dbname     = getenv("DB_NAME") ?: 'attendance';
+    $username   = getenv("DB_USER") ?: 'admin';
+    $password   = getenv("DB_PASS") ?: '1111';
+}
 
 // สร้างการเชื่อมต่อ
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -10,7 +28,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // ตรวจสอบการเชื่อมต่อ
 if ($conn->connect_error) {
     die("การเชื่อมต่อฐานข้อมูลล้มเหลว: " . $conn->connect_error);
-}else{
-    #echo "Connect Successfully";
 }
-?>
